5408ebb31a153c8be9f37c20936a473eda3b9236
thu105 <thu105@mail.chapman.edu>
2016-11-29 13:23:39 -0800

    added documentations

--- PlayerSync.java
+++ PlayerSync.java
@@ -1,10 +1,11 @@
 public class PlayerSync{
-  public volatile boolean endStage;
+  public volatile boolean endStage;//a sentinel value to end chat servers
   public volatile int threadDone;//counter to make sure every thread finished the stages
   public volatile int currentStage;//1=day chat, 2=lynch, 3=night chat, 4=night action, 5=explanation, 6=game over
-  public volatile int mafiaMember, innocentMember, mostVote;
-  public volatile int vote[];
-  public volatile String deadInitial;
+  public volatile int mafiaMember, innocentMember;//amount of players alive
+  public volatile int mostVote;//index of player who received most vote
+  public volatile int vote[];//will contain votes and also actions chosen by players
+  public volatile String deadInitial;//will contain username of players who left the game
 
   public PlayerSync(){
     endStage=false;
