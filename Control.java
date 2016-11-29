a627cee9ffcaa1885394216cc77d36a102532c8a
thu105 <thu105@mail.chapman.edu>
2016-11-29 13:50:06 -0800

    Added Documentation

--- Control.java
+++ Control.java
@@ -1,3 +1,8 @@
+/**
+* Control.java
+*
+* This class is shared by SWTClient and ClientListener to set a end of chat server stage
+*/
 public class Control{
   public volatile boolean end;
   public Control(){
